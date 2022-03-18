dc1b5b2287f48b4c2c0ec2a818c973772d9f1ecd
User <User@imac-deni-2.lan>
2022-03-18 21:50:55 +0700

    coba push

--- jawaban-latihan/jawaban-function/index.js
+++ jawaban-latihan/jawaban-function/index.js
@@ -1,69 +1,3 @@
-// FUnction
-
-// function hello(name) {
-//     console.log("Hello, " + name)
-// }
-
-// invoke function / call function
-// hello("admin")
-
-/**Study Case 1
- * 
- * Tentukan faktor-faktor dari sebuah angka.
- * 
- * Contoh:
- * 
- * function factor(n){
- *  // code here
- * }
- * 
- * Result:
- * factor(10)
- * // 1 2 5 10
- */
-
-// function factor(n) {
-//     for(var i = 1; i <= n; i++) {
-//         if(n % i ===0){
-//             console.log(i)
-//         }
-//     }
-// }
-
-// factor(1000000);
-
-/**STudy Case 2 
- * 
- * Buat 2 function untuk cek apakah sebuah bilangan Prima atau tidak.
- * 
- * Task:
- * - function checkFactor(n) untuk menentukan jumlah faktornya
- * - function checkPrime(n) untuk cek bilangan prima atau tidak (main function) 
-*/
-
-// function checkFactor(n) {
-//     let countFactor = 0;
-//     for (var i = 1; i <= n; i++) {
-//         if (n % i === 0) {
-//             countFactor++;
-//         }
-//     }
-//     return countFactor;
-// }
-
-// // Pengertian Bilangan Prima : bilangan yang hanya memiliki 2 faktor: 1 dan bilangan itu sendiri
-// function checkPrime(n) {
-//     let jumlahFaktor = checkFactor(n);
-//     if (jumlahFaktor === 2) {
-//         return "Bilangan Prima"
-//     } else {
-//         return "Bilangan Bukan Prima / Bilangan Komposit"
-//     }
-// }
-
-// console.log(checkPrime(5))
-
-
 /** Task 1 : Bilangan Prima 
  * Print N bilangan angka prima pertama
  * 
@@ -113,7 +47,6 @@ while ( i <= num) {
 console.log(tempArray);
 }
 
-
 console.log("------------- ");
 console.log(" ");
 console.log("Task 1:")
@@ -121,7 +54,6 @@ console.log(" ");
 printNumbers(5)
 console.log(" ");
 console.log("------------- ");
-
 
 
 /** Task 2 : Triple Pythagoras
@@ -176,126 +108,54 @@ console.log("------------- ");
  */
 
 
-// Test # 1
-// ------------------------------------
-// var input = "5279312";
-// var input = "313282787";
-// var input = "19";
-// var input = "7";
-// var input = "";
-
-// var angka1 = 0;
-// var angka2 = 0;
-// var besar = 0;
-// var hasil = "";
-
-// if (input.length = 1) {
-//     input = "00" + input;
-// }
-
-// for (var i = 0; i < input.length-1; i++) {
-//     if (i % 2 === 0) {
-// //        console.log(input[i]+input[i+1]);
-//         angka1 = Number(input[i] + input [i+1]);
-//         console.log("angka1 = " + angka1);
-//     } else {
-// //        console.log(input[i]+input[i+1]);
-//         angka2 = Number(input[i] + input [i+1]);
-//         console.log("angka2 = " + angka2);
-//     }
-    
-// if (besar < angka1) {
-//     besar = angka1;
-// } else {
-//     if (besar < angka2) {
-//     besar = angka2;
-//     }
-// }    
-
-// }
-
-// if (besar <= 0) {
-//     hasil = "Input number";
-// } else {
-//     hasil = (besar).toString();
-// }
-
-// console.log(" ");
-// console.log("Task 3:")
-// console.log(" ");
-// console.log("besar = " + besar);
-// console.log("hasil = " + hasil);
-// console.log(" ");
-// console.log("------------- ");
-
-
-// Test # 2
-// ------------------------------------
-// var input = "5279312";
-// var input = "313282787";
-// var input = "19";
-// var input = "7";
-// var input = "";
-
-// var angka1 = 0;
-// var angka2 = 0;
-// var besar = 0;
-// var hasil = "";
-
-function splitToArray(input) {
-    let angka = 0;
-    let besar = 0;
-    let hasil = "";
-
-if (input.length = 1) {
-    input = "00" + input;
-    }
-
-for (var i = 0; i < input.length-1; i++) {
-    if (i % 2 === 0) {
-//        console.log(input[i]+input[i+1]);
-        angka[i] = Number(input[i] + input [i+1]);
-        console.log("angka" + "("+i+") = " + angka[i]);
-        return angka[i];
-    } else {
-//      console.log(input[i]+input[i+1]);
-        angka[i] = Number(input[i] + input [i+1]);
-        console.log("angka" + "("+i+") = " + angka[i]);
-        return angka[i];
-    }
-}
-}
-
-function findTheLargest(input) {
-    let besar = 0;
-    let hasil = "";
-    let arrayNumbers = splitToArray(input);
-for (var i = 0; i < input.length-1; i++) {
-    if (i % 2 === 0) {
-    if (besar < angka[i]) {
-        besar = angka[i];
-    } else {
-        if (besar > angka[i]) {
-        besar = angka[i];
-    }
-    if (besar <= 0) {
-        hasil = "Input number";
-    } else {
-        hasil = (besar).toString();
-    }
-    return hasil;
-}
-}
-
-
 console.log(" ");
 console.log("Task 3:");
 console.log(" ");
-// console.log("besar = " + besar);
-// console.log("hasil = " + hasil);
-console.log(splitToArray("5279312"));
-// console.log(findTheLargest("5279312"));
+
+function splitToArray(input) {
+let tempStr = "";
+let tempAngka = [];
+let i = 0;
+
+if (input.length <= 1) {
+    input = " " + input;
+}
+
+while (i < input.length-1) {
+    tempStr = input[i] + input[i+1];
+    tempAngka.push(Number(tempStr));
+    i++;
+}
+return tempAngka
+}
+
+function findTheLargest(input) {
+let tempNum = 0;
+let tempStr = "";
+let scores = splitToArray(input);
+let i = 0;
+
+while (i < scores.length) {
+    if (scores[i+1] > scores[i]) {
+        tempNum = scores[i+1];
+    } 
+    i++;
+}
+if (i <= 1) {
+    tempNum = scores[0];
+}
+tempStr = String(tempNum);   
+if (scores[0] === undefined) {
+    tempStr = "Input number";
+}
+return tempStr;
+}
+
+console.log(findTheLargest("5275789312"));
+console.log(findTheLargest("313282787"));
+console.log(findTheLargest("19"));
+console.log(findTheLargest("7"));
+console.log(findTheLargest(""));
 
 console.log(" ");
 console.log("------------- ");
-
